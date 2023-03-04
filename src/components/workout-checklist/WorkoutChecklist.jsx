ac7fe5b6ec4d45e061e8b08a468a6965cc43a44f
Michael <michaelphan350@gmail.com>
2023-03-04 11:04:46 -0700

    completed outline for workout section

--- src/components/workout-checklist/WorkoutChecklist.jsx
+++ src/components/workout-checklist/WorkoutChecklist.jsx
@@ -1,10 +1,20 @@
 // import Stack from 'react-bootstrap/Stack';
+import { Dropdown, DropdownButton } from "react-bootstrap";
 import ThemeProvider from 'react-bootstrap/ThemeProvider';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container, Row, Col } from 'react-bootstrap';
 import "./workoutChecklist.scss";
+import React, {useState} from 'react';
 
-function workoutChecklist () {
+function WorkoutChecklist () {
+
+    const [selectedOption, setSelectedOption] = useState(null);
+    const [dropdownTitle, setDropdownTitle] = useState('Training Weeks:');
+    const handleSelect = (eventKey, event) => {
+        setSelectedOption(eventKey);
+        setDropdownTitle (event.target.innerText);
+    }
+
     return (
         <ThemeProvider
         breakpoints = {['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
@@ -12,12 +22,32 @@ function workoutChecklist () {
     >
         <Container>
             <Row>
-                <Col>1</Col>
-                <Col>2</Col>
+                <Col className="weekDropdownContainer">
+                    <DropdownButton id='dropdown-basic-button' className="dropdownMenu" title={dropdownTitle} onSelect={handleSelect}>
+                        <Dropdown.Item eventKey='option1'>Weeks 1-4</Dropdown.Item>
+                        <Dropdown.Item eventKey='option2'>Weeks 5-8</Dropdown.Item>
+                        <Dropdown.Item eventKey='option3'>Weeks 9-12</Dropdown.Item>
+                    </DropdownButton>
+                    {selectedOption === "option1" && (
+                        <div>
+                            <p>Hello</p>
+                        </div>
+                    )}
+                    {selectedOption === "option2" && (
+                        <div>
+                            <p>Goodbye</p>
+                        </div>
+                    )}
+                    {selectedOption === "option3" && (
+                        <div>
+                            <p>Welcome</p>
+                        </div>
+                    )}
+                </Col>
             </Row>
         </Container>
     </ThemeProvider>
     )
 }
 
-export default workoutChecklist;
+export default WorkoutChecklist;
