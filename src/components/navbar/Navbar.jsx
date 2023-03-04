5971c7c7e435f2cf95afeae683271ed601a752f8
Michael <michaelphan350@gmail.com>
2023-03-04 10:33:15 -0700

    finished navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,51 @@
+// import Stack from 'react-bootstrap/Stack';
+import Sun from "../assets/images/sun_moon.jpeg";
+// import Button from "react-bootstrap/Button";
+import React, { useState, useEffect } from 'react'
+import ThemeProvider from 'react-bootstrap/ThemeProvider'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container, Row, Col } from 'react-bootstrap'
+import './navbar.scss';
 
 function Navbar() {
+
+    const [theme, setTheme] = useState('dark');
+    const toggleTheme = () => {
+        if (theme === 'dark') {
+            setTheme('light')
+        } else {
+            setTheme('dark')
+        }
+    }
+
+    useEffect(() => {
+        document.body.className = theme;
+    }, [theme])
+
   return (
-    <Container>
-    <Row className='flex-wrap'>
-        <Col>1</Col>
-        <Col>2</Col>
-    </Row>
-    </Container>
+    <ThemeProvider
+        breakpoints = {['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
+        minBreakpoint = "xxs"
+    >
+        <Container className='navbarContainer'>
+            <Row className='navContentsContainer'>
+                <Col lg={2} className="btnContainer">
+                    <button onClick={toggleTheme}>
+                        <img src={Sun} alt="moon/sun img" />
+                    </button>
+                </Col>
+                <Col lg={8}className="titleContainer">
+                    <h1>Military Training Regimen</h1>
+                </Col>
+                <Col lg={2} className="menuContainer">
+                    <ul>
+                        <li>About</li>
+                        <li>Contacts</li>
+                    </ul>
+                </Col>
+            </Row>
+        </Container>
+    </ThemeProvider>
   );
 }
 
--- src/components/workout-checklist/WorkoutChecklist.jsx
+++ src/components/workout-checklist/WorkoutChecklist.jsx
@@ -1,10 +1,22 @@
+// import Stack from 'react-bootstrap/Stack';
+import ThemeProvider from 'react-bootstrap/ThemeProvider';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container, Row, Col } from 'react-bootstrap';
 import "./workoutChecklist.scss";
 
 function workoutChecklist () {
     return (
-        <div>
-            <p>Hello Everyone</p>
-        </div>
+        <ThemeProvider
+        breakpoints = {['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
+        minBreakpoint = "xxs"
+    >
+        <Container>
+            <Row>
+                <Col>1</Col>
+                <Col>2</Col>
+            </Row>
+        </Container>
+    </ThemeProvider>
     )
 }
 
